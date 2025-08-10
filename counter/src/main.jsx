0064cedd85300530eb9840e3fcfb73e2c33062bd
Manasi170806 <manasipatel17082006@gmail.com>
2025-08-11 05:17:05 +0530

    10/08/2025

--- counter/src/main.jsx
+++ counter/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from '../src/store/store';
 import App from './App';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
