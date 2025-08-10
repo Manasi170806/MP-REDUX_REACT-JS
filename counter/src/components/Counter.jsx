1db59d8ef7271695196e48c992d9ee25a7a6659d
Manasi Patel <manasipatel17082006@gmail.com>
2025-08-11 05:20:10 +0530

    Update Counter.jsx

--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,5 +1,3 @@
-// src/components/Counter.jsx
-
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from "../features/counterSlice";
 
@@ -8,12 +6,13 @@ function Counter() {
   const dispatch = useDispatch();
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h2>Redux Toolkit Counter</h2>
-      <h1>{count}</h1>
-
-      <button onClick={() => dispatch(increment())}>+1</button>
-      <button onClick={() => dispatch(decrement())}>-1</button>
+    <div className="counter-container">
+      <h3>Redux Toolkit Counter</h3>
+      <div className="counter-value">{count}</div>
+      <div className="counter-buttons">
+        <button onClick={() => dispatch(decrement())}>-1</button>
+        <button onClick={() => dispatch(increment())}>+1</button>
+      </div>
     </div>
   );
 }
