f601d3e4459250699cf26c7d2809fb2009b18809
Manasi170806 <manasipatel17082006@gmail.com>
2025-08-21 23:08:48 +0530

    done

--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from "../features/counterSlice";
 
+
 function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
--- counter/src/main.jsx
+++ counter/src/main.jsx
@@ -5,6 +5,7 @@ import { store } from '../src/store/store';
 import App from './App';
 import './App.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
